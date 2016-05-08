73fe8a1e90c528b6a45953e971adbe7ec8ff2ef2
Vlad Vlaskin <vlaskinvlad@gmail.com>
2016-05-08 13:31:20 +0200

    [Some fun with docker swarm and compose]

---
+++ env-azure.sh
@@ -0,0 +1,4 @@
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://13.74.185.45:2376"
+export DOCKER_CERT_PATH="/Users/vlad/repo/ansible-cloud-hosting/certs/docker"
+export DOCKER_MACHINE_NAME="azure"
